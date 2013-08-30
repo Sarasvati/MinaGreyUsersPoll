594835e632d83c35fe77266ec0ff3b93282081ed
Nicla Rossini <fineart.sarasvati@gmail.com>
2013-08-30 14:01:35 +0200

    Update poll.php

--- poll.php
+++ poll.php
@@ -23,7 +23,7 @@
  ************************************************************************************           
  *  As long as you keep all notices intact, you can do what you want with this.     *
  *               If you modify it, add your name and a date                         *
- ************************************************************************************
+ ************************************************************************************/
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
